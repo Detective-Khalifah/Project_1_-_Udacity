6bc04473f60b1293f684e6b22268236fefc2a44a
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-03-18 01:29:55 +0100

    Use findViewById() to map buttons; define free kick methods for both teams

--- app/src/main/java/com/blogspot/thengnet/imagi_football/MainActivity.java
+++ app/src/main/java/com/blogspot/thengnet/imagi_football/MainActivity.java
@@ -3,17 +3,34 @@ package com.blogspot.thengnet.imagi_football;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    boolean penaltyKicked = true;
+
+    private Button btnA1, btnB1, // "+1 point" score buttons for both teams
+            btnAFoul, btnBFoul, // "Foul" buttons for both teams
+            btnAFree, btnBFree; // "Free Kick" buttons for both teams
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnA1 = (Button) findViewById(R.id.btn_one_a);
+        btnB1 = (Button) findViewById(R.id.btn_b_one);
+
+        btnAFoul = (Button) findViewById(R.id.btn_a_foul);
+        btnBFoul = (Button) findViewById(R.id.btn_b_foul);
+
+        btnAFree = (Button) findViewById(R.id.btn_a_free);
+        btnBFree = (Button) findViewById(R.id.btn_b_free);
     }
 
     /**
@@ -32,12 +49,60 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB();
     }
 
+    /**
+     * Generate a random boolean value to determine if "Team A" scores +1 point after the free kick
+     * then enable all buttons with exception of the "Free Kick" buttons.
+     * the @param vi #freeKickForTeamA button
+     */
     public void freeKickForTeamA(View vi) {
+        penaltyKicked = true;
 
+        Random freeForA = new Random();
+        boolean freeKickWinned = freeForA.nextBoolean();
+
+        if (freeKickWinned) {
+            scoreTeamA++;
+        }
+
+        btnA1.setEnabled(true);
+        btnB1.setEnabled(true);
+        btnAFoul.setEnabled(true);
+        btnBFoul.setEnabled(true);
+
+        // Disable the "Free Kick" buttons from being clicked
+        btnAFree.setEnabled(false);
+        btnBFree.setEnabled(false);
+
+        // Display Team A's current score after the free kick
+        displayForTeamA();
     }
 
+    /**
+     * Generate a random boolean value to determine if "Team B" scores +1 point after the free kick
+     * then enable all buttons with exception of the "Free Kick" buttons.
+     * the @param vi #freeKickForTeamB button
+     */
     public void freeKickForTeamB(View vi) {
+        penaltyKicked = true;
 
+        Random freeForB = new Random();
+        boolean freeKickWinned = freeForB.nextBoolean();
+
+        if (freeKickWinned) {
+            scoreTeamB++;
+        }
+
+        btnA1.setEnabled(true);
+        btnB1.setEnabled(true);
+        btnAFoul.setEnabled(true);
+        btnBFoul.setEnabled(true);
+
+        // Disable the "Free Kick" buttons from being clicked
+        btnAFree.setEnabled(false);
+        btnBFree.setEnabled(false);
+
+        // Display Team A's current score after the free kick
+        displayForTeamA();
     }
 
     public void teamAFouls(View vi) {
